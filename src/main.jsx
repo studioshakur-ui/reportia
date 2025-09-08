ebde57bdfb48f5b7170177bf00eb56641dcbed52
studioshakur-ui <studio.shakur@gmail.com>
2025-09-08 21:18:17 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import './index.css';
+
 
 // Désactive les Service Workers (évite écran noir iOS quand cache corrompu)
 if ("serviceWorker" in navigator) {
