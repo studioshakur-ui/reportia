48e7a2009d7779c4fdd6e4113e4b655218dce802
studioshakur-ui <studio.shakur@gmail.com>
2025-09-08 14:55:27 +0200

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
-import "./index.css";
+import ReactDOM from "react-dom/client";
+import App from "./App";
 
-createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// Désactive les Service Workers (évite écran noir iOS quand cache corrompu)
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker.getRegistrations().then(regs => regs.forEach(r => r.unregister()));
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
