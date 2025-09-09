e174976955fa7b79a6c1da236643c4d60b75d214
studioshakur-ui <studio.shakur@gmail.com>
2025-09-09 15:01:16 +0200

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: { port: 5173 },
-  build: {
-    target: ['es2020','edge88','firefox78','chrome87','safari14']
-  }
 })
